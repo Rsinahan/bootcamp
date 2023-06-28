e5ad3fb323e3a81591d95f757e0fcf66cdeda902
Rsinahan <romelsinahan04@gmail.com>
2023-06-28 12:25:28 +0800

    add contact title on contact pages

---
+++ HtmlTopics/index.js
@@ -0,0 +1,8 @@
+/*let message;
+message = 'Hello World!';
+    alert(message);
+    console.log(message);*/
+    let hello = 'Welcome to our First Page!';
+    let message = hello;
+    alert(hello);
+    alert(message);

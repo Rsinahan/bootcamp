0b1c3f3c877bd9e45bbd3bc2cf7a79e29f3f569a
Rsinahan <romelsinahan04@gmail.com>
2023-07-06 22:51:41 +0800

    add homepage contents

--- HtmlTopics/index.js
+++ HtmlTopics/index.js
@@ -2,7 +2,25 @@
 message = 'Hello World!';
     alert(message);
     console.log(message);*/
-    let hello = 'Welcome to our First Page!';
+   /* let hello = 'Welcome to our First Page!';
     let message = hello;
     alert(hello);
-    alert(message);
+    alert(message);*/
+
+   /* let name = prompt('Who are you?','');
+    alert(`You are ${name}`);
+    let x = 1, y = 3;
+    alert(x - y );*/
+
+    let accessAllowed;
+    let age = prompt ('How old are you?','');
+
+        if (age > 18) {
+            accessAllowed = true;
+        } else { accessAllowed = false; 
+        }
+
+        alert(accessAllowed)
+
+
+    
